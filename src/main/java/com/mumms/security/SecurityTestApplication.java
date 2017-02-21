e0e1cbdfec740afb41fec94dc752ebd72b571637
Lev Shulman <lshulman@mumms.com>
2017-02-21 14:04:38 -0700

    testing with no session

--- src/main/java/com/mumms/security/SecurityTestApplication.java
+++ src/main/java/com/mumms/security/SecurityTestApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 public class SecurityTestApplication {
@@ -33,20 +31,20 @@ public class SecurityTestApplication {
 
 	}
 	
-	@Configuration
-	@EnableRedisHttpSession
-	public class SessionConfig {
-		@Bean
-		public JedisConnectionFactory connectionFactory() {
-			String redisHost = "redis", redisPort = "6379";
-
-			JedisConnectionFactory factory = new JedisConnectionFactory();
-			//if either of these are null, the app will just fail to startup -- which is actually what we want
-			factory.setHostName(redisHost);
-			factory.setPort(new Integer(redisPort) );
-			return factory;
-		}
-	}
+//	@Configuration
+//	@EnableRedisHttpSession
+//	public class SessionConfig {
+//		@Bean
+//		public JedisConnectionFactory connectionFactory() {
+//			String redisHost = "redis", redisPort = "6379";
+//
+//			JedisConnectionFactory factory = new JedisConnectionFactory();
+//			//if either of these are null, the app will just fail to startup -- which is actually what we want
+//			factory.setHostName(redisHost);
+//			factory.setPort(new Integer(redisPort) );
+//			return factory;
+//		}
+//	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityTestApplication.class, args);
